52bd86e2171465f812ae242ef4ec5bbbcda78397
Dominik Plíšek <Dominik@Dominiks-MacBook-Pro.local>
2013-06-25 21:30:32 +0200

    Initial Commit

---
+++ FordFulkerson/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FordFulkerson
+//
+//  Created by Dominik Plíšek on 6/25/13.
+//  Copyright (c) 2013 Dominik Plíšek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
